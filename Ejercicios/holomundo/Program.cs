730ed40f91d4bda556bfa16843e54d8be23c35b2
Bryan ZavMur <bf.murillo@unah.hn>
2021-02-23 16:33:44 -0600

    agregando hola mundo

---
+++ Ejercicios/holomundo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace holomundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! in spanish is Hola Mundo!");
+        }
+    }
+}
