b1ef7ac944f1e8366898935949145cec6db76820
Bryan ZavMur <bf.murillo@unah.hn>
2021-03-08 21:32:10 -0600

    Subiendo tarea 2

---
+++ Ejercicios/6.Funciones/funciones/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace funciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Alumno a = new Alumno(1, "Bryan", "Zavala");
+            Alumno b = new Alumno(2, "Waldina", "Zavala");
+
+            a.inactivarAlumno();
+
+            Console.WriteLine(a.nombreCompleto());
+            Console.WriteLine(b.nombreCompleto());
+
+            a.activarAlumno();
+
+            Console.WriteLine(" ");
+            
+            Console.WriteLine(a.nombreCompleto());
+            Console.WriteLine(b.nombreCompleto());
+
+
+        }
+    }
+}
